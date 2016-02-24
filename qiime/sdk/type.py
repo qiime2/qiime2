74c9907b6bfa3d10afd46a914c9a961bd7d2b3e3
ebolyen <ebolyen@gmail.com>
2016-02-23 17:01:40 -0700

    RENAME THIS COMMIT

--- qiime/sdk/type.py
+++ qiime/sdk/type.py
@@ -6,6 +6,55 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 # ----------------------------------------------------------------------------
 
+class MetaType(type):
+    def __getitem__(cls, arg):
+        typetree = TypeTree(arg)
+        #memoize here ideally
+        return self.__class__('TypeExpr', (_Type, _TypeExpr), {'__typetree__': typetree})
 
-class Type(object):
+class _TypeExpr(metaclass=Final):
+    __typetree__ = TypeTree()
+
+class _Type(metaclass=MetaType):
+    Interfaces = tuple()
+
+    @classmethod
+    def serialize(cls, directory, subcontexts):
+        pass
+
+    @classmethod
+    def deserialize(cls, directory, subcontexts):
+        pass
+
+class Type(_Type):
     pass
+
+class ArtifactType(TypeTree):
+    pass
+
+# Some file-------------------------------------
+
+
+
+class MySubInterface(Interface):
+    def my_method(self):
+        pass
+
+class MySubInterface2(Interface):
+    pass
+
+class MyArtifactType(ArtifactType):
+    Interfaces = (MySubInterface, MySubInterface2)
+
+    @classmethod
+    def serialize(cls, directory, contexts):
+        int1, int2 = contexts
+        int1.my_method(directory.create_file())
+
+    @classmethod
+    def deserialize(cls, directory):
+        pass
+
+
+class Flooble(ArtifactType):
+    Interfaces = tuple(MyThingy)
