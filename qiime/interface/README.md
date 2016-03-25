5f9aa95b2b0300baa56b891a61cdb15a040a1dea
Greg Caporaso <gregcaporaso@gmail.com>
2016-03-25 06:47:58 -0700

    ENH/MAINT: fixes #32

--- qiime/interface/README.md
+++
@@ -1,5 +0,0 @@
-# QIIME 2 interfaces
-
-This is a temporary subpackage for QIIME 2 interface prototypes. The interfaces
-in this subpackage will be moved to separate repositories in the future and
-this subpackage will be removed.
--- qiime/interface/web/__init__.py
+++
@@ -1,7 +0,0 @@
-# ----------------------------------------------------------------------------
-# Copyright (c) 2016--, QIIME 2 development team.
-#
-# Distributed under the terms of the Modified BSD License.
-#
-# The full license is in the file COPYING.txt, distributed with this software.
-# ----------------------------------------------------------------------------
