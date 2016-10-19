32b124e4792b6f9db3a8cd3c1cc08a413289abc7
Evan Bolyen <ebolyen@gmail.com>
2016-10-19 15:04:06 -0700

    ENH: Signature now matches order of function sig. (#170)
    Previously the `inputs` and `parameters` property of `Signature` objects
    created by the `.from_function` alternate constructor did not respect the order
    of the original function signature. Now these properties are always
    `collections.OrderedDict` instances.

--- qiime/core/type/tests/test_signature.py
+++ qiime/core/type/tests/test_signature.py
@@ -9,7 +9,9 @@
 import unittest
 import collections
 
-from qiime.core.type.signature import PipelineSignature
+from qiime.core.type import Visualization
+from qiime.core.type.signature import (
+    PipelineSignature, MethodSignature, VisualizerSignature)
 from qiime.plugin import Int
 from qiime.core.testing.type import IntSequence1
 
@@ -56,6 +58,107 @@ class TestPipelineSignature(unittest.TestCase):
             collections.OrderedDict([
                 ('output1', (IntSequence1, list)),
                 ('output2', (IntSequence1, list))]))
+
+
+class TestMethodSignature(unittest.TestCase):
+    def test_from_function(self):
+        def method(input3: list, input2: dict, in1: set, param1: str,
+                   p2: int=2) -> tuple:
+            pass
+
+        inputs = {
+            'in1': IntSequence1,
+            'input2': IntSequence1,
+            'input3': IntSequence1
+        }
+        exp_inputs = collections.OrderedDict([
+            ('input3', (IntSequence1, list)),
+            ('input2', (IntSequence1, dict)),
+            ('in1', (IntSequence1, set))
+        ])
+
+        parameters = {
+            'param1': Int,
+            'p2': Int
+        }
+        exp_parameters = collections.OrderedDict([
+            ('param1', (Int, str)),
+            ('p2', (Int, int))
+        ])
+
+        outputs = [
+            ('out', IntSequence1)
+        ]
+        exp_outputs = collections.OrderedDict([
+            ('out', (IntSequence1, tuple))
+        ])
+
+        exp_defaults = {
+            'p2': 2
+        }
+
+        sig = MethodSignature.from_function(
+            method, inputs, parameters, outputs)
+
+        self.assertIsInstance(sig.inputs, collections.OrderedDict)
+        self.assertEqual(sig.inputs, exp_inputs)
+
+        self.assertIsInstance(sig.parameters, collections.OrderedDict)
+        self.assertEqual(sig.parameters, exp_parameters)
+
+        self.assertIsInstance(sig.outputs, collections.OrderedDict)
+        self.assertEqual(sig.outputs, exp_outputs)
+
+        self.assertEqual(sig.defaults, exp_defaults)
+
+
+class TestVisualizerSignature(unittest.TestCase):
+    def test_from_function(self):
+        def visualizer(output_dir, input3: list, input2: dict, in1: set,
+                       param1: str, p2: int=2) -> None:
+            pass
+
+        inputs = {
+            'in1': IntSequence1,
+            'input2': IntSequence1,
+            'input3': IntSequence1
+        }
+        exp_inputs = collections.OrderedDict([
+            ('input3', (IntSequence1, list)),
+            ('input2', (IntSequence1, dict)),
+            ('in1', (IntSequence1, set))
+        ])
+
+        parameters = {
+            'param1': Int,
+            'p2': Int
+        }
+        exp_parameters = collections.OrderedDict([
+            ('param1', (Int, str)),
+            ('p2', (Int, int))
+        ])
+
+        exp_outputs = collections.OrderedDict([
+            ('visualization', (Visualization, None))
+        ])
+
+        exp_defaults = {
+            'p2': 2
+        }
+
+        sig = VisualizerSignature.from_function(
+            visualizer, inputs, parameters)
+
+        self.assertIsInstance(sig.inputs, collections.OrderedDict)
+        self.assertEqual(sig.inputs, exp_inputs)
+
+        self.assertIsInstance(sig.parameters, collections.OrderedDict)
+        self.assertEqual(sig.parameters, exp_parameters)
+
+        self.assertIsInstance(sig.outputs, collections.OrderedDict)
+        self.assertEqual(sig.outputs, exp_outputs)
+
+        self.assertEqual(sig.defaults, exp_defaults)
 
 
 if __name__ == "__main__":
