128a4d5b7c4de520e89f36334fe0139888490319
Daniel McDonald <mcdonadt@colorado.edu>
2016-11-02 11:24:13 -0700

    STY: removed whitespace

--- qiime/core/type/signature.py
+++ qiime/core/type/signature.py
@@ -315,7 +315,7 @@ def function_to_signature(cls, function, inputs, parameters, outputs):
     output_types = collections.OrderedDict()
     for output_name, semantic_type in outputs.items():
         view_type = output_view_types.get(output_name)
-        
+
         if view_type is None:
             raise TypeError("Function %r does not describe %r in its return "
                             "annotation." % (function.__name__, output_name))
