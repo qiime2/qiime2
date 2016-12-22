f1a995d690639b974371a1e52c0bf4de7688e9c5
Jai Ram Rideout <jai.rideout@gmail.com>
2016-12-21 17:23:26 -0700

    MAINT: use standard copyright/license header
    Most/all copyright/license headers in .py files incorrectly referenced
    COPYING.txt instead of LICENSE. A few files didn't have a copyright header at
    all.
    
    Fixes #124.

--- qiime2/core/testing/method.py
+++ qiime2/core/testing/method.py
@@ -1,9 +1,9 @@
 # ----------------------------------------------------------------------------
-# Copyright (c) 2016--, QIIME 2 development team.
+# Copyright (c) 2016-2017, QIIME 2 development team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
-# The full license is in the file COPYING.txt, distributed with this software.
+# The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
 import qiime2
