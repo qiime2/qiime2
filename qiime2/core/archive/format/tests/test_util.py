240a269ed37817c4b919b233b0f68bf42a5bfd78
Matthew Dillon <matthewrdillon@gmail.com>
2017-01-09 09:44:26 -0700

    ENH: Adds basic provenance parsing support
    parse_actions is a private utility helper that accepts v0- and v1- format
    Artifacts, and returns a NetworkX DiGraph where the nodes represent artifacts
    and the edges represent actions. The nodes and edges each carry the necessary
    attributes (as extracted from the provenance) to recreate the execution
    pipeline necessary to derive the  artifact being processed.
    This utility is intended to be a stop-gap measure for now, and we anticipate
    replacing it with a more full-featured provenance object.

--- qiime2/core/archive/format/tests/test_util.py
+++ qiime2/core/archive/format/tests/test_util.py
@@ -10,10 +10,12 @@ import tempfile
 import os
 import zipfile
 
-from qiime2.core.testing.type import FourInts
-from qiime2.core.testing.util import ArchiveTestingMixin
+import networkx as nx
+
+from qiime2.core.testing.type import FourInts, IntSequence1
+from qiime2.core.testing.util import ArchiveTestingMixin, get_dummy_plugin
 import qiime2.core.archive as archive
-from qiime2.core.archive.format.util import artifact_version
+from qiime2.core.archive.format.util import artifact_version, parse_actions
 from qiime2.sdk import Artifact
 
 
@@ -77,3 +79,87 @@ class TestArtifactVersion(unittest.TestCase, ArchiveTestingMixin):
         with zipfile.ZipFile(fp, mode='r') as zf:
             version = zf.read(os.path.join(root_dir, 'VERSION'))
         self.assertRegex(str(version), '^.*archive: 1.*$')
+
+
+class TestParseProvenance(unittest.TestCase, ArchiveTestingMixin):
+    def setUp(self):
+        prefix = "qiime2-test-temp-"
+        self.plugin = get_dummy_plugin()
+        self.temp_dir = tempfile.TemporaryDirectory(prefix=prefix)
+        self.provenance_capture = archive.ImportProvenanceCapture()
+
+    def test_parse_artifact_v0(self):
+        fp = os.path.join(self.temp_dir.name, 'artifact_v0.qza')
+
+        with artifact_version(0):
+            artifact = Artifact._from_view(FourInts, [-1, 42, 0, 43], list,
+                                           self.provenance_capture)
+            artifact.save(fp)
+
+        provenance = parse_actions(artifact)
+        self.assertIsInstance(provenance, nx.DiGraph)
+        # Check structure
+        self.assertEqual(nx.number_of_nodes(provenance), 1)
+        self.assertEqual(nx.number_of_edges(provenance), 0)
+        # Check node attrs
+        self.assertFalse(provenance.node[artifact.uuid])  # empty dict
+
+    def test_parse_artifact_v1(self):
+        fp = os.path.join(self.temp_dir.name, 'artifact_v1.qza')
+
+        with artifact_version(1):
+            artifact = Artifact._from_view(FourInts, [-1, 42, 0, 43], list,
+                                           self.provenance_capture)
+            artifact.save(fp)
+
+        provenance = parse_actions(artifact)
+        self.assertIsInstance(provenance, nx.DiGraph)
+        # Check structure
+        self.assertEqual(nx.number_of_nodes(provenance), 1)
+        self.assertEqual(nx.number_of_edges(provenance), 0)
+        # Check node attrs
+        self.assertEqual(set(provenance.node[artifact.uuid]),
+                         {'manifest', 'format', 'type'})
+
+    def test_parse_two_nodes(self):
+        fp = os.path.join(self.temp_dir.name, 'left.qza')
+        initial = Artifact._from_view(IntSequence1, [-1, 42, 0, 43], list,
+                                      self.provenance_capture)
+        left, _ = self.plugin.methods['split_ints'](initial)
+        left.save(fp)
+
+        provenance = parse_actions(left)
+        self.assertIsInstance(provenance, nx.DiGraph)
+        # Check structure
+        self.assertEqual(nx.number_of_nodes(provenance), 2)
+        self.assertEqual(nx.number_of_edges(provenance), 1)
+        # Check directionality
+        self.assertEqual(provenance.successors(initial.uuid), [left.uuid])
+        self.assertEqual(provenance.predecessors(left.uuid), [initial.uuid])
+        # Check edge attrs
+        self.assertEqual(set(provenance[initial.uuid][left.uuid]),
+                         {'parameters', 'plugin', 'action', 'type'})
+
+    def test_parse_three_nodes(self):
+        fp = os.path.join(self.temp_dir.name, 'viz.qzv')
+        initial = Artifact._from_view(IntSequence1, [-1, 42, 0, 43], list,
+                                      self.provenance_capture)
+        left, _ = self.plugin.methods['split_ints'](initial)
+        viz, = self.plugin.visualizers['most_common_viz'](left)
+        viz.save(fp)
+
+        provenance = parse_actions(viz)
+        self.assertIsInstance(provenance, nx.DiGraph)
+        # Check structure
+        self.assertEqual(nx.number_of_nodes(provenance), 3)
+        self.assertEqual(nx.number_of_edges(provenance), 2)
+        # Check directionality
+        self.assertEqual(provenance.successors(initial.uuid), [left.uuid])
+        self.assertEqual(provenance.successors(left.uuid), [viz.uuid])
+        self.assertEqual(provenance.predecessors(viz.uuid), [left.uuid])
+        self.assertEqual(provenance.predecessors(left.uuid), [initial.uuid])
+        # Check edge attrs
+        self.assertEqual(set(provenance[initial.uuid][left.uuid]),
+                         {'parameters', 'plugin', 'action', 'type'})
+        self.assertEqual(set(provenance[left.uuid][viz.uuid]),
+                         {'parameters', 'plugin', 'action', 'type'})
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     url='https://qiime2.org',
     packages=find_packages(),
     install_requires=['pyyaml', 'decorator', 'pandas', 'cookiecutter',
-                      'tzlocal', 'python-dateutil'],
+                      'tzlocal', 'python-dateutil', 'networkx'],
     entry_points={
         'qiime2.plugins': [
             'dummy-plugin=qiime2.core.testing.plugin:dummy_plugin'
