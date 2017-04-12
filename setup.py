31b5c249a4c7dc2fe6ca2b47c352b53e6f68b5f4
Jai Ram Rideout <jai.rideout@gmail.com>
2017-04-12 10:27:06 -0700

    MAINT: template recipe (#1)

--- setup.py
+++ setup.py
@@ -17,8 +17,6 @@ setup(
     license='BSD-3-Clause',
     url='https://qiime2.org',
     packages=find_packages(),
-    install_requires=['pyyaml', 'decorator', 'pandas', 'tzlocal',
-                      'python-dateutil'],
     entry_points={
         'qiime2.plugins': [
             'dummy-plugin=qiime2.core.testing.plugin:dummy_plugin'
