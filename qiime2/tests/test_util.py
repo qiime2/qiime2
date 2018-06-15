18789d192bcb3fd0d85f7ea2f4117bceae1be1a6
Chris Keefe <39198770+ChrisKeefe@users.noreply.github.com>
2018-06-15 11:56:41 -0700

    BUG: duplicate will copy if not owner (#406)

--- qiime2/tests/test_util.py
+++ qiime2/tests/test_util.py
@@ -7,14 +7,16 @@
 # ----------------------------------------------------------------------------
 
 import os
+import errno
 import tempfile
 import unittest
 import unittest.mock as mock
 
 import qiime2.util as util
 
-EXDEV = OSError(18, "Invalid cross-device link")
-EPERM = PermissionError(1, "find this")
+EXDEV = OSError(errno.EXDEV, "Invalid cross-device link")
+EPERM = PermissionError(errno.EPERM, "unsafe operation e.g. using link wrong")
+EACCES = PermissionError(errno.EACCES, "insufficient r/w permissions")
 SECRET = "this is a secret for testing, don't tell anyone!"
 
 
@@ -59,12 +61,22 @@ class TestDuplicate(unittest.TestCase):
         with self.assertRaisesRegex(IsADirectoryError, self.dir):
             util.duplicate(self.src, self.dir)
 
-    @mock.patch('qiime2.util.os.link', side_effect=EPERM)
-    def test_perm_error(self,  mocked_link):
-        with self.assertRaisesRegex(PermissionError, "find this"):
+    @mock.patch('qiime2.util.os.link', side_effect=EACCES)
+    def test_perm_error_EACCES(self, mocked_link):
+        with self.assertRaisesRegex(
+                PermissionError, "insufficient r/w permissions"):
             util.duplicate(self.src, self.dst1)
 
         assert mocked_link.called
+
+    @mock.patch('qiime2.util.os.link', side_effect=EPERM)
+    def test_perm_error_EPERM(self, mocked_link):
+        util.duplicate(self.src, self.dst1)
+
+        assert mocked_link.called
+        assert os.path.exists(self.dst1)
+        with open(self.dst1) as fh:
+            self.assertEqual(fh.read(), SECRET)
 
     @mock.patch('qiime2.util.os.link', side_effect=EXDEV)
     def test_cross_device_src_not_exists(self, mocked_link):
@@ -96,9 +108,10 @@ class TestDuplicate(unittest.TestCase):
             util.duplicate(self.src, self.dir)
 
     @mock.patch('qiime2.util.os.link', side_effect=EXDEV)
-    @mock.patch('qiime2.util.shutil.copyfile', side_effect=EPERM)
+    @mock.patch('qiime2.util.shutil.copyfile', side_effect=EACCES)
     def test_cross_device_perm_error(self, mocked_link, mocked_copyfile):
-        with self.assertRaisesRegex(PermissionError, "find this"):
+        with self.assertRaisesRegex(
+                PermissionError, "insufficient r/w permissions"):
             util.duplicate(self.src, self.dst1)
 
         assert mocked_link.called
