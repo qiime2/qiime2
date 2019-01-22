bc4ff558768694be02a19969f95ae8efd6f5b3ff
Turan Naimey <turan1naimey@gmail.com>
2019-01-22 10:50:12 -0700

    MAINT: Updating copyright headers for 2019 (#426)

--- qiime2/plugin/model/__init__.py
+++ qiime2/plugin/model/__init__.py
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------------
-# Copyright (c) 2016-2018, QIIME 2 development team.
+# Copyright (c) 2016-2019, QIIME 2 development team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
