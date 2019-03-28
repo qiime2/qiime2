955132a8ebca6a8500fabfd941f72b3433851bf9
Matthew Dillon <matthewrdillon@gmail.com>
2019-03-27 19:47:16 -0700

    SQUASH: :lint:

--- qiime2/core/type/template.py
+++ qiime2/core/type/template.py
@@ -1,3 +1,10 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016-2019, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
 
 from abc import ABCMeta, abstractmethod
 import itertools
