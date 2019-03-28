f8890e341fdabb286c9d1896877f29a10be1d2af
Sleightly <ejc042@ucsd.edu>
2019-03-27 17:08:59 -0700

    Current version of typemap without random path added yet

--- qiime2/sdk/typemap.py
+++ qiime2/sdk/typemap.py
@@ -14,6 +14,61 @@ q2p = qiime2.sdk.PluginManager()
 #    print(value.methods.keys())
 ##############testing#################
 
+def getNextParam(method, input=True):
+    """Get a tuple of required/nonrequired inputs or outputs for each method
+
+    Parameters
+    ----------
+    method : Qiime2.method (check the right signature for this)
+        The method to get the semantic types from 
+    input : {True,False}
+        Delineates if getting the method input or output types
+
+    Returns
+    -------
+    Tuple - list of required and optional semantic types
+
+    """
+
+    req = []
+    non_req = []
+
+    if input:
+        for k,v in method.signature.inputs.items():
+            if not v.has_default():
+                req.append(v.qiime_type)
+            else:
+                non_req.append(v.qiime_type)
+    else:
+        for k,v in method.signature.outputs.items():
+            if not v.has_default():
+                req.append(v.qiime_type)
+            else:
+                non_req.append(v.qiime_type)
+    return req, non_req
+
+def getCombinations(no_req):
+    """Get all permutations of list of semantic types
+
+    Parameters
+    ----------
+    non_req : list of semantic types
+
+    Returns
+    -------
+    List of lists - each list contains an instance of a permutation
+
+    """
+
+    if not no_req:
+        return no_req
+    no_req_comb = []
+    for i in range(len(no_req)+1):
+        combs = combinations(no_req, i)
+        for c in combs:
+            no_req_comb.append(list(c))
+    return no_req_comb
+
 
 
 class TypeMap:
@@ -25,63 +80,7 @@ class TypeMap:
     def __init__(self):
         self.G = nx.DiGraph()
 
-    def getNextParam(method, input=True):
-        """Get a tuple of required/nonrequired inputs or outputs for each method
-
-        Parameters
-        ----------
-        method : Qiime2.method (check the right signature for this)
-            The method to get the semantic types from 
-        input : {True,False}
-            Delineates if getting the method input or output types
-
-        Returns
-        -------
-        Tuple - list of required and optional semantic types
-
-        """
-    
-        req = []
-        non_req = []
-
-        if input:
-            for k,v in method.signature.inputs.items():
-                if not v.has_default():
-                    req.append(v.qiime_type)
-                else:
-                    non_req.append(v.qiime_type)
-        else:
-            for k,v in method.signature.outputs.items():
-                if not v.has_default():
-                    req.append(v.qiime_type)
-                else:
-                    non_req.append(v.qiime_type)
-        return req, non_req
-
-    def getCombinations(no_req):
-        """Get all permutations of list of semantic types
-    
-        Parameters
-        ----------
-        non_req : list of semantic types
-
-        Returns
-        -------
-        List of lists - each list contains an instance of a permutation
-
-        """
-
-        if not no_req:
-            return no_req
-        no_req_comb = []
-        for i in range(len(no_req)+1):
-            combs = combinations(no_req, i)
-            for c in combs:
-                no_req_comb.append(list(c))
-        return no_req_comb
-
-
-    def buildGraph():
+    def buildGraph(self):
         """Constructs a networkx graph with different semantic types 
         and methods as nodes
     
@@ -94,17 +93,17 @@ class TypeMap:
                 req_out, non_req_out = getNextParam(method,0)
             
                 for key in req_in:
-                    if not G.has_node(key):
-                        G.add_node(key, value=key, color='blue')
+                    if not self.G.has_node(key):
+                        self.G.add_node(key, value=key, color='blue')
                 for key in non_req_in:
-                    if not G.has_node(key):
-                        G.add_node(key, value=key, color='blue')
+                    if not self.G.has_node(key):
+                        self.G.add_node(key, value=key, color='blue')
                 for key in req_out:
-                    if not G.has_node(key):
-                        G.add_node(key, value=key, color='blue')
+                    if not self.G.has_node(key):
+                        self.G.add_node(key, value=key, color='blue')
                 for key in non_req_out:
-                    if not G.has_node(key):
-                        G.add_node(key, value=key, color='blue')
+                    if not self.G.has_node(key):
+                        self.G.add_node(key, value=key, color='blue')
 
                 #there are combinations
                 if non_req_in:
@@ -123,24 +122,31 @@ class TypeMap:
                             new_method_key += ";"+non_req_key
                     
                     #check if there exists a method node with same in-edges
-                        if not G.has_node(new_method_key):
-                            G.add_node(new_method_key,value=method,color='red')
+                        if not self.G.has_node(new_method_key):
+                            self.G.add_node(new_method_key,value=method,color='red')
                             for key in req_in:
-                                G.add_edge(key, new_method_key)
+                                self.G.add_edge(key, new_method_key)
                             for key in non_req:
-                                G.add_edge(key, new_method_key)
+                                self.G.add_edge(key, new_method_key)
                             for key in req_out:
-                                G.add_edge(new_method_key, key)
+                                self.G.add_edge(new_method_key, key)
                             for key in non_req_out:
-                                G.add_edge(new_method_key, key)
+                                self.G.add_edge(new_method_key, key)
                 else:
-                    if not G.has_node(method):
-                        G.add_node(method,value=method,color='red')
+                    if not self.G.has_node(method):
+                        self.G.add_node(method,value=method,color='red')
                         for key in req_in:
-                            G.add_edge(key, method)
+                            self.G.add_edge(key, method)
                         for key in req_out:
-                            G.add_edge(method, key)
+                            self.G.add_edge(method, key)
                         for key in non_req_out:
-                            G.add_edge(method, key)
+                            self.G.add_edge(method, key)
+                            
+
+
+
+
+
+
 
 
