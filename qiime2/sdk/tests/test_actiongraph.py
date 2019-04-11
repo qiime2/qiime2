8ed2f97719b7943b266d589464658677d7074604
Sleightly <ejc042@ucsd.edu>
2019-04-11 13:36:09 -0700

    adding test for methods with trivial input

--- qiime2/sdk/tests/test_actiongraph.py
+++ qiime2/sdk/tests/test_actiongraph.py
@@ -16,6 +16,44 @@ class TestUtil(unittest.TestCase):
     def test_build_graph(self):
         #set up graph to contain multiple methods
         methods = ['long_description_method', 'no_input_method', 
+                   'params_only_method', 'docstring_order_method'] 
+    
+        all_methods = list(self.plugin.methods.keys())
+        for method in all_methods:
+            if method not in methods:
+                del self.plugin.methods[method]
+
+        #test 
+        self.g = build_graph()
+        obs = list(self.g.nodes)
+        exp = [Mapping, str(self.plugin.methods['no_input_method']), 
+               str(self.plugin.methods['long_description_method']),
+               str(self.plugin.methods['params_only_method']),
+               str(self.plugin.methods['docstring_order_method']), 
+               str(self.plugin.methods['docstring_order_method'])+';'+str(Mapping)]
+
+        for item in exp:
+            assert item in obs
+        
+        docstring_node = str(self.plugin.methods['docstring_order_method'])
+        docstring_node_2 = str(self.plugin.methods['docstring_order_method'])+';'+str(Mapping)
+        long_node = str(self.plugin.methods['long_description_method'])
+        params_node = str(self.plugin.methods['params_only_method'])
+        no_input_node = str(self.plugin.methods['no_input_method'])
+        type_node = Mapping
+
+        assert self.g.has_edge(docstring_node, type_node)
+        assert self.g.has_edge(type_node, docstring_node)
+        assert self.g.has_edge(docstring_node_2, type_node)
+        assert self.g.has_edge(type_node, docstring_node)
+        assert self.g.has_edge(long_node, type_node)
+        assert self.g.has_edge(type_node, long_node)
+        assert self.g.has_edge(params_node, type_node)
+        assert self.g.has_edge(no_input_node, type_node)
+    
+
+        #set up graph to contain multiple methods
+        methods = ['long_description_method', 'no_input_method', 
                    'params_only_method'] 
     
         all_methods = list(self.plugin.methods.keys())
