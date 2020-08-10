d7aa02fb9c01bbab17d5eafcd1f08fa8491a25a2
Andrew Sanchez <inbox.asanchez@gmail.com>
2020-08-10 14:15:04 -0700

    revert

--- qiime2/sdk/__init__.py
+++ qiime2/sdk/__init__.py
@@ -12,10 +12,8 @@ from .plugin_manager import PluginManager
 from .result import Result, Artifact, Visualization
 from .results import Results
 from .util import parse_type, parse_format, type_from_ast
-from .usage import UsageAction, UsageInputs, UsageOutputNames
 from ..core.cite import Citations
 
 __all__ = ['Result', 'Results', 'Artifact', 'Visualization', 'Action',
            'Method', 'Visualizer', 'Pipeline', 'PluginManager', 'parse_type',
-           'parse_format', 'type_from_ast', 'Context', 'Citations',
-           'UsageAction', 'UsageInputs', 'UsageOutputNames']
+           'parse_format', 'type_from_ast', 'Context', 'Citations']
