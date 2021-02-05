66ea209e136fc2e1a69268cdba81d02e1130d137
q2d2 <q2d2.noreply@gmail.com>
2021-02-05 00:41:45 +0000

    MAINT: Updating copyright year

--- qiime2/core/cite.py
+++ qiime2/core/cite.py
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------------
-# Copyright (c) 2016-2020, QIIME 2 development team.
+# Copyright (c) 2016-2021, QIIME 2 development team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
