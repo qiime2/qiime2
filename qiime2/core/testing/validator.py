87c34051c610becc9a4a26d6b484554f1246e342
Keegan Evans <keegan.evans@gmail.com>
2021-08-04 13:48:58 -0700

    more header fixes

--- qiime2/core/testing/validator.py
+++ qiime2/core/testing/validator.py
@@ -1,3 +1,11 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016-2021, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
 from .type import Kennel, Dog, Cat
 from .plugin import dummy_plugin
 
