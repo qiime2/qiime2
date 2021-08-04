bad4d3b18a1f76119a5bfdc1d94395a3878c46ac
Keegan Evans <keegan.evans@gmail.com>
2021-08-04 13:47:15 -0700

    maint: fix copyright on validate.py

--- qiime2/core/validate.py
+++ qiime2/core/validate.py
@@ -6,16 +6,21 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
-def get_validators(concrete_type, validators):
-    return validators[concrete_type]
+class SemanticValidation:
+    def __init__(self, concrete_type):
+       self._validators = super.validators
+       self._concrete_type = concrete_type
 
-def _sort_validators(validators):
-    """does nothing right now"""
-    return validators
+    def get_validators(self, concrete_type, validators):
+        return validators[concrete_type]
 
-def run_validators(concrete_type, validators):
-    type_validators = get_validators(concrete_type, validators)
-    sorted_validators = _sort_validators(type_validators)
-
-    for validator in sorted_validators:
-        validator.validator(view=validator.view)
+    def _sort_validators(self, validators):
+        """does nothing right now"""
+        return validators
+    
+    def run_validators(self, concrete_type, validators):
+        type_validators = self.get_validators(concrete_type, validators)
+        sorted_validators = self._sort_validators(type_validators)
+    
+        for validator in sorted_validators:
+            validator.validator(view=validator.view)
