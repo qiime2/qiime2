622315b6229a4bcc050dee3e29f7657535f1ab16
Anthony Simard <als872@nau.edu>
2021-09-14 14:13:04 -0700

    SQUASH: Remove trash

--- runinfo/1005/submit_scripts/parsl.adhoc.1631560164.3584003.sh
+++
@@ -1,2 +0,0 @@
-
-process_worker_pool.py  --max_workers=6 -a anthony-ThinkPad-T490,10.18.35.35,127.0.0.1,134.114.101.52 -p 0 -c 1.0 -m None --poll 10 --task_port=54213 --result_port=54410 --logdir=/home/anthony/src/qiime2/qiime2/htex --block_id=0 --hb_period=30  --hb_threshold=120 --cpu-affinity none 
--- runinfo/1006/submit_scripts/parsl.adhoc.1631560200.2006388.sh
+++
@@ -1,2 +0,0 @@
-
-process_worker_pool.py  --max_workers=6 -a 134.114.101.52,anthony-ThinkPad-T490,10.18.35.35,127.0.0.1 -p 0 -c 1.0 -m None --poll 10 --task_port=54845 --result_port=54702 --logdir=/home/anthony/src/qiime2/qiime2/htex --block_id=0 --hb_period=30  --hb_threshold=120 --cpu-affinity none 
