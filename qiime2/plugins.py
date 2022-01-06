4a6b35ee9c27bc4974bb40d4ab49d03ac8b9a2eb
Chris Keefe <crk239@nau.edu>
2022-01-05 19:17:18 -0700

    lstrips leading periods from file extension strings

--- qiime2/plugins.py
+++ qiime2/plugins.py
@@ -178,7 +178,7 @@ class ArtifactAPIUsage(usage.Usage):
 
     def init_format(self, name, factory, ext=None):
         if ext is not None:
-            name = '%s.%s' % (name, ext)
+            name = '%s.%s' % (name, ext.lstrip('.'))
 
         variable = super().init_format(name, factory, ext=ext)
 
