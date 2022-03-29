a16d332edfee695919d4ce18f424255847926573
Anthony <als872@nau.edu>
2022-03-29 12:05:50 -0700

    SQUASH: Add comment about accessing pool to context

--- qiime2/sdk/context.py
+++ qiime2/sdk/context.py
@@ -88,6 +88,8 @@ class Scope:
         """Add a reference to something destructable that is owned by this
            scope.
         """
+        # The pool is probably going to be referred to by the thread local
+        # config, we should be able to access that here
         self._locals.append(ref)
 
     def add_parent_reference(self, ref):
