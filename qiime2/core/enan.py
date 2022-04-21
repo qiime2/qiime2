0eff975f34734d2d7acbffb860299a40dd35ae67
Evan Bolyen <ebolyen@gmail.com>
2022-04-21 16:35:33 -0700

    squash: missing license header

--- qiime2/core/enan.py
+++ qiime2/core/enan.py
@@ -1,3 +1,11 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016-2022, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
 import struct
 import pandas as pd
 
