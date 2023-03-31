c2e2b5a1849d8ad505621141bbc0f2b17fa71905
Anthony <als872@nau.edu>
2023-03-31 12:12:42 -0700

    SQUASH: Turns out symlinks don't exist

--- qiime2/core/cache.py
+++ qiime2/core/cache.py
@@ -1493,7 +1493,7 @@ class Pool:
         # already exists. This could happen legitimately from trying to save
         # the same thing to a named pool several times.
         with self.cache.lock:
-            if not os.path.exists(dest):
+            if not os.path.islink(dest):
                 os.symlink(src, dest)
 
     def _rename_to_collection_pool(self, uuid, src):
