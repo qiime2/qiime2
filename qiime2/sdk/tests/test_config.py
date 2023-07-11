5fb12a0f8385a08aea8bd537e654193da5701668
Anthony <als872@nau.edu>
2023-07-11 10:38:41 -0700

    Refactor where state is created for test

--- qiime2/sdk/tests/test_config.py
+++ qiime2/sdk/tests/test_config.py
@@ -25,80 +25,85 @@ from qiime2.sdk.parallel_config import (PARALLEL_CONFIG, ParallelConfig,
 
 
 class TestConfig(unittest.TestCase):
+    # Get actions
+    plugin = get_dummy_plugin()
+    pipeline = plugin.pipelines['resumable_pipeline']
+    method = plugin.methods['list_of_ints']
+
+    # Create configs
+    config = parsl.Config(
+        executors=[
+            ThreadPoolExecutor(
+                max_threads=1,
+                label='default'
+            ),
+            HighThroughputExecutor(
+                label='htex',
+                max_workers=1,
+                provider=LocalProvider()
+            )
+        ],
+        # AdHoc Clusters should not be setup with scaling strategy.
+        strategy='none',
+    )
+
+    tpool_default = parsl.Config(
+        executors=[
+            ThreadPoolExecutor(
+                max_threads=1,
+                label='default'
+            ),
+            HighThroughputExecutor(
+                label='htex',
+                max_workers=1,
+                provider=LocalProvider()
+            )
+        ],
+        # AdHoc Clusters should not be setup with scaling strategy.
+        strategy='none',
+    )
+
+    htex_default = parsl.Config(
+        executors=[
+            ThreadPoolExecutor(
+                max_threads=1,
+                label='tpool'
+            ),
+            HighThroughputExecutor(
+                label='default',
+                max_workers=1,
+                provider=LocalProvider()
+            )
+        ],
+        # AdHoc Clusters should not be setup with scaling strategy.
+        strategy='none',
+    )
+
+    # Expected provenance based on type of executor used
+    tpool_expected = [{
+        'type': 'parsl', 'parsl_type': 'ThreadPoolExecutor'}, {
+        'type': 'parsl', 'parsl_type': 'ThreadPoolExecutor'}]
+    htex_expected = [{
+        'type': 'parsl', 'parsl_type': 'HighThroughputExecutor'}, {
+        'type': 'parsl', 'parsl_type': 'HighThroughputExecutor'}]
+
     def setUp(self):
         # Ensure default state prior to test
         PARALLEL_CONFIG.parallel_config = None
         PARALLEL_CONFIG.action_executor_mapping = {}
 
-        plugin = get_dummy_plugin()
-        self.pipeline = plugin.pipelines['resumable_pipeline']
-        self.method = plugin.methods['list_of_ints']
-
-        # Create temp test dir
+        # Create temp test dir and cache in dir
         self.test_dir = tempfile.TemporaryDirectory(prefix='qiime2-test-temp-')
+        self.cache = Cache(os.path.join(self.test_dir.name, 'new_cache'))
+
+        # Create artifacts here so we have unique inputs in each test
+        self.art = [Artifact.import_data(SingleInt, 0),
+                    Artifact.import_data(SingleInt, 1)]
+
+        # Get paths to config files
         self.mapping_config_fp = self.get_data_path('mapping_config.toml')
         self.mapping_only_config_fp = self.get_data_path(
             'mapping_only_config.toml')
-
-        # Create artifact and cache
-        self.art = [Artifact.import_data(SingleInt, 0),
-                    Artifact.import_data(SingleInt, 1)]
-        self.cache = Cache(os.path.join(self.test_dir.name, 'new_cache'))
-
-        self.config = parsl.Config(
-            executors=[
-                ThreadPoolExecutor(
-                    max_threads=1,
-                    label='default'
-                ),
-                HighThroughputExecutor(
-                    label='htex',
-                    max_workers=1,
-                    provider=LocalProvider()
-                )
-            ],
-            # AdHoc Clusters should not be setup with scaling strategy.
-            strategy='none',
-        )
-
-        self.tpool_default = parsl.Config(
-            executors=[
-                ThreadPoolExecutor(
-                    max_threads=1,
-                    label='default'
-                ),
-                HighThroughputExecutor(
-                    label='htex',
-                    max_workers=1,
-                    provider=LocalProvider()
-                )
-            ],
-            # AdHoc Clusters should not be setup with scaling strategy.
-            strategy='none',
-        )
-
-        self.htex_default = parsl.Config(
-            executors=[
-                ThreadPoolExecutor(
-                    max_threads=1,
-                    label='tpool'
-                ),
-                HighThroughputExecutor(
-                    label='default',
-                    max_workers=1,
-                    provider=LocalProvider()
-                )
-            ],
-            # AdHoc Clusters should not be setup with scaling strategy.
-            strategy='none',
-        )
-
-        self.tpool_expected = [{
-            'type': 'parsl', 'parsl_type': 'ThreadPoolExecutor'}, {
-            'type': 'parsl', 'parsl_type': 'ThreadPoolExecutor'}]
-        self.htex_expected = [{
-            'type': 'parsl', 'parsl_type': 'HighThroughputExecutor'}, {
-            'type': 'parsl', 'parsl_type': 'HighThroughputExecutor'}]
 
     def tearDown(self):
         self.test_dir.cleanup()
