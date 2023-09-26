db01dc8b6001c133c0c2471542bc0434e4b90c39
colinvwood <woodcolin38@gmail.com>
2023-09-26 11:06:26 -0700

    add copyrights

--- qiime2/core/archive/provenance_lib/tests/test_archive_parser.py
+++ qiime2/core/archive/provenance_lib/tests/test_archive_parser.py
@@ -1,3 +1,10 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016-2023, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
 from datetime import timedelta
 import os
 import networkx as nx
