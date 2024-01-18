14ac218a56b80657a67b6bfa5abf010fe4e3a1dc
Oddant1 <oddant1@hotmail.com>
2024-01-18 14:28:32 -0700

    No double import os

--- qiime2/core/path.py
+++ qiime2/core/path.py
@@ -24,11 +24,9 @@ def _party_parrot(self, *args):
 
 class OwnedPath(_ConcretePath):
     def __new__(cls, *args, **kwargs):
-        import os
         from qiime2.core.cache import get_cache
 
         cache = get_cache()
-
         kwargs['prefix'] = os.path.join(cache.process_pool.path,
                                         'q2-%s-' % cls.__name__)
 
