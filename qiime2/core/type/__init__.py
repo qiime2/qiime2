f104801c8414cc44a8ed8849f54362900e340988
Colin Wood <woodcolin38@gmail.com>
2024-01-31 11:51:31 -0700

    parallel type helper

--- qiime2/core/type/__init__.py
+++ qiime2/core/type/__init__.py
@@ -19,13 +19,14 @@ from .meta import TypeMap, TypeMatch
 from .util import (is_primitive_type, is_semantic_type, is_metadata_type,
                    is_collection_type, is_visualization_type,
                    interrogate_collection_type, parse_primitive,
-                   is_union, is_metadata_column_type)
+                   is_union, is_metadata_column_type, is_parallel_type)
 
 __all__ = [
     # Type Helpers
     'is_semantic_type', 'is_visualization_type', 'is_primitive_type',
     'is_metadata_type', 'is_collection_type', 'interrogate_collection_type',
     'parse_primitive', 'is_union', 'is_metadata_column_type',
+    'is_parallel_type',
     # Collection Types
     'Set', 'List', 'Collection',
     # Semantic Types
