e378fe1901e1e7d7b77794cf7294bc19f886e291
q2d2 <q2d2.noreply@gmail.com>
2025-01-04 01:05:13 +0000

    Auto-update copyright year to 2025

--- qiime2/core/type/tests/test_util.py
+++ qiime2/core/type/tests/test_util.py
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------------
-# Copyright (c) 2016-2023, QIIME 2 development team.
+# Copyright (c) 2016-2025, QIIME 2 development team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
