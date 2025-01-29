4053db26b176477653877e5359d0101e70ae8c5d
Liz Gehret <elizabeth.gehret@gmail.com>
2025-01-29 14:42:19 -0700

    adds cpu flags into action.yaml & rebases off of dev

--- qiime2/core/archive/provenance.py
+++ qiime2/core/archive/provenance.py
@@ -21,6 +21,7 @@ from datetime import datetime, timezone
 from typing import Any, List, NamedTuple, Set, Union
 from pathlib import Path
 
+import cpuinfo
 import distutils
 import yaml
 import tzlocal
@@ -457,6 +458,7 @@ class ProvenanceCapture:
     def make_env_section(self):
         env = collections.OrderedDict()
         env['platform'] = platform.platform()
+        env['cpu-flags'] = cpuinfo.get_cpu_info().get('flags', [])
         # There is a trailing whitespace in sys.version, strip so that YAML can
         # use literal formatting.
         env['python'] = LiteralString('\n'.join(line.strip() for line in
