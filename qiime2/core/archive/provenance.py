c24a914df37e1c22dfeef13b25b580e770ee56f7
Liz Gehret <elizabeth.gehret@gmail.com>
2025-01-30 13:58:30 -0700

    let's see if this fixes the long runtime

--- qiime2/core/archive/provenance.py
+++ qiime2/core/archive/provenance.py
@@ -20,7 +20,7 @@ from datetime import datetime, timezone
 from typing import Any, List, NamedTuple, Set, Union
 from pathlib import Path
 
-import cpuinfo
+# import cpuinfo
 import distutils
 import yaml
 import tzlocal
@@ -455,7 +455,7 @@ class ProvenanceCapture:
     def make_env_section(self):
         env = collections.OrderedDict()
         env['platform'] = pkg_resources.get_build_platform()
-        env['cpu-flags'] = cpuinfo.get_cpu_info().get('flags', [])
+        # env['cpu-flags'] = cpuinfo.get_cpu_info().get('flags', [])
         # There is a trailing whitespace in sys.version, strip so that YAML can
         # use literal formatting.
         env['python'] = LiteralString('\n'.join(line.strip() for line in
