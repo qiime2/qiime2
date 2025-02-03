4cb2d6437d297dc457a52b163a1c8af01101f664
Liz Gehret <elizabeth.gehret@gmail.com>
2025-02-03 16:44:20 -0700

    fix version parsing to allow for floats

--- qiime2/core/archive/provenance_lib/util.py
+++ qiime2/core/archive/provenance_lib/util.py
@@ -53,7 +53,7 @@ def get_nonroot_uuid(fp: pathlib.Path) -> str:
 
 _VERSION_MATCHER = (
     r'QIIME 2\n'
-    r'archive: [0-9]{1,2}$\n'
+    r'archive: [0-9]+(?:\.[0-9]+)?$\n'
     r'framework: '
     r'(?:20[0-9]{2}|2)\.(?:[1-9][0-2]?|0)\.[0-9](?:\.dev[0-9]?)?'
     r'(?:\+[.\w]+)?\Z'
