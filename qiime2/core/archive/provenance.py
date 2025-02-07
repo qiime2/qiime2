d40264149dcebff922140d4104898b253971408b
Liz Gehret <elizabeth.gehret@gmail.com>
2025-02-06 17:08:34 -0700

    update datadir method inside provcapture

--- qiime2/core/archive/provenance.py
+++ qiime2/core/archive/provenance.py
@@ -42,27 +42,6 @@ def _ts_to_date(ts):
     return datetime.fromtimestamp(ts, tz=time_zone)
 
 
-def _get_datadir_size(self):
-    data_fp = self._archiver.data_dir
-
-    total_size = sum(path.stat().st_size for path in data_fp.iterdir()
-                     if path.is_file())
-    datadir_size = humanize.naturalsize(total_size, binary=True)
-
-    action_yaml_path = self.action_dir / 'action.yaml'
-
-    with action_yaml_path.open('r') as fp:
-        action_yaml = yaml.safe_load(fp)
-
-    execution = action_yaml.get('execution', {})
-    execution['datadir-size'] = datadir_size
-    action_yaml['execution'] = execution
-
-    # Write back the modified YAML
-    with action_yaml_path.open('w') as fp:
-        yaml.safe_dump(action_yaml, fp, default_flow_style=False, indent=4)
-
-
 # Used to give PyYAML something to recognize for custom tags
 ForwardRef = collections.namedtuple('ForwardRef', ['reference'])
 NoProvenance = collections.namedtuple('NoProvenance', ['uuid'])
@@ -362,6 +341,27 @@ class ProvenanceCapture:
                         shutil.copytree(str(grandcestor), str(destination))
 
         return str(artifact.uuid)
+
+    def _get_datadir_size(self):
+        # TODO: correct path for data dir
+        data_fp = artifact._archiver.data_dir
+
+        total_size = sum(path.stat().st_size for path in data_fp.iterdir()
+                         if path.is_file())
+        datadir_size = humanize.naturalsize(total_size, binary=True)
+
+        action_yaml_path = self.action_dir / 'action.yaml'
+
+        with action_yaml_path.open('r') as fp:
+            action_yaml = yaml.safe_load(fp)
+
+        execution = action_yaml.get('execution', {})
+        execution['datadir-size'] = datadir_size
+        action_yaml['execution'] = execution
+
+        # Write back the modified YAML
+        with action_yaml_path.open('w') as fp:
+            yaml.safe_dump(action_yaml, fp, default_flow_style=False, indent=4)
 
     def make_citation_key(self, domain, package=None, identifier=None,
                           index=0):
