e5263aabdb8211cc3cc43153b09ad995d605e47b
Liz Gehret <elizabeth.gehret@gmail.com>
2025-09-24 13:37:35 -0700

    docstrings for Format v7.1

---
+++ qiime2/core/archive/format/v7_1.py
@@ -0,0 +1,67 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016-2025, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+import qiime2.core.archive.format.v7_0 as v7_0
+
+
+class ArchiveFormat(v7_0.ArchiveFormat):
+    """QIIME 2 Archive Format Version 7.1
+
+    New Features
+    ------------
+
+    Annotation[Signature]
+        `Signature` has been added as a new supported Annotation sub-type.
+
+        This sub-type acts as a mechanism for users to 'self-sign' a Result.
+
+        A new Signature can be created and attached to a Result either via
+        the Python API or the CLI.
+
+        `annotations` directory structure (containing an example Signature):
+
+            annotations/
+            ├── uuid1/
+            │   ├── metadata.yaml
+            │   ├── signature.txt
+            │   ├── checksums.sha256
+
+        The `metadata.yaml` file for Signatures will contain all standard
+        Annotation fields, plus Signature-specific fields:
+
+        Standard fields:
+            `id`
+            `name`
+            `type`
+            `created_at`
+            `root_result_uuid`
+            `referenced_result_uuid`
+
+        Signature-specific fields:
+
+            `algorithm`
+                The algorithm used to compute the signature.
+                Ed25519 is currently the only supported algorithm in 7.1.
+
+            `fingerprint`
+                The calculated sha256 checksum of the public key.
+
+            `manifest_digest`
+                The calculated sha256 checksum of the Signature-level
+                checksums.sha256 file.
+
+            `signer_name`
+                (Optional) Name of the user creating the Signature.
+
+            `signer_email`
+                (Optional) Email address of the user creating the Signature.
+
+        The `signature.txt` file will contain the Ed25519-calculated signature
+        of the private key and the Signature-level checksums.sha256 file.
+    """
+    pass
