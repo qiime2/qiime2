7ec9327be24a8dcd5acf22a11006413bf6f20513
Colin Wood <68213641+colinvwood@users.noreply.github.com>
2026-07-28 11:52:36 -0700

    Update src/rachis/sdk/tests/test_artifact.py
    Co-authored-by: Anthony Simard <als872@nau.edu>

--- src/rachis/sdk/tests/test_artifact.py
+++ src/rachis/sdk/tests/test_artifact.py
@@ -408,6 +408,7 @@ class TestArtifact(unittest.TestCase, ArchiveTestingMixin):
         nested = test_dir / '.dotdir' / 'nested'
         nested.touch()
 
+        # If dotfiles weren't ignored, this would fail.
         Artifact.import_data(IntSequence1, test_dir)
 
     def test_import_data_ignores_dotpaths_df(self):
